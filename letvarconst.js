43ffa31ad99c972f5b74421378baf1ade1c6cd65
darshan <darshanj590@gmail.com>
2026-02-10 13:57:17 +0530

    added the new code

--- letvarconst.js
+++ letvarconst.js
@@ -1,2 +1,17 @@
+//var -> function scope based
+
 var a = 10;
+var a = 15;
 console.log(a);
+
+//let -> blocked scope
+
+let a1 = 6;
+//let a1 = 10;
+console.log(a1);
+
+//const -> blocked scope 
+
+const a2 = 100;
+
+console.log(a2);
